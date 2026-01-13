88af3a7298d67e26cc7391b897b727c8a1ceff46
BaeZzi813 <woehd8252@gmail.com>
2026-01-13 23:14:36 +0900

    Refact: Navbar 로고 css로 분기 처리

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,18 +4,21 @@ import logoImg from "@/public/logo.svg";
 import logofullImg from "@/public/logofull.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { useMediaQuery } from "react-responsive";
 
 export default function Navbar() {
-  const isMobile = useMediaQuery({ query: "(max-width: 767px )" });
   return (
     <>
       <div className="border-b border-b-slate-200 h-15 flex items-center ">
         <div className="lg:w-300 lg:mx-auto">
           <Link href="/" className="w-fit flex items-center">
             <Image
-              className="ml-4 md:ml-6 lg:ml-0"
-              src={isMobile ? logoImg : logofullImg}
+              className="ml-4 md:ml-6 lg:ml-0 block md:hidden"
+              src={logoImg}
+              alt="로고 이미지"
+            />
+            <Image
+              className="ml-4 md:ml-6 lg:ml-0 hidden md:block"
+              src={logofullImg}
               alt="로고 이미지"
             />
           </Link>
